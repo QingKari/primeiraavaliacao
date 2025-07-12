3c5a1413057780a0b81d9b2065b6b01c4fef76b1
QingKari <wellissonsilva200@gmail.com>
2025-07-12 20:58:30 -0300

    Criar atividade 004_parte2.java

---
+++ atividade 004_parte2.java
@@ -0,0 +1,13 @@
+public class ComparacaoIdades {
+    public static void main(String[] args) {
+        // Declarando as idades
+        int idadePessoa1 = 25;
+        int idadePessoa2 = 30;
+
+        // Comparando as idades
+        boolean pessoa1MaisVelha = idadePessoa1 > idadePessoa2;
+
+        // Exibindo o resultado
+        System.out.println("A primeira pessoa é mais velha: " + pessoa1MaisVelha);
+    }
+}
